7997dbba605039b779115a9c6d4c5fa7ab8de687
edvaz8 <135179033+edvaz8@users.noreply.github.com>
2024-01-01 19:54:44 -0600

    static routes and flask application
    setting flask tags for routes

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Lógica adicional si es necesaria para obtener datos para la página index
+    return render_template('users-index.html')
+
+@app.route('/profile')
+def profile():
+    # Lógica adicional si es necesaria para obtener datos del perfil
+    return render_template('users-profile.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
